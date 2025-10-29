79dd73a6629666b7647be7ea87f0f9e5ab64b1fc
Soffia Yañez <jsyanezg@udistrital.edu.co>
2025-10-28 19:38:24 -0500

    Add files via upload

---
+++ Programa_2.c
@@ -0,0 +1,19 @@
+ //Programa_2.c
+//  Valerie Hernandez
+// Soffia Yañez
+//17 de octubre del 2025
+//asiganción de variables  de datos
+
+#include <stdio.h> // sirve para  mostrar cosas en pantalla o leer datos. 
+int main() { //Aquí empieza el programa.
+short enteroNumero1 = 115;//Es un numero entero
+signed int enteroNumero2 = 55;//Para  números que pueden ser positivos o negativos.
+unsigned long enteroNumero3 = 789;//Guarda el número 789 en una cajita grande que solo acepta números positivos.
+char caracterA = 65;//Asigna una cadena de caracteres
+char caracterB = 'B';//
+float puntoFlotanteNumero1 = 89.8;// Muestra decimales
+ double puntoFlotanteNumero2 = 238.2236;
+return 0;
+}
+
+ 
